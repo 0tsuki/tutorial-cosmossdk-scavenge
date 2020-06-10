d397423773c9ac07dfe246214197ebb05c8baabd
OTSUKI Shinsuke <otsuki.shinsuke@gmail.com>
2020-06-10 09:24:22 +0900

    step04: Imple MsgCreateScavenge

---
+++ x/scavenge/types/MsgCreateScavenge.go
@@ -0,0 +1,47 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+// verify interface at compile time
+var _ sdk.Msg = &MsgCreateScavenge{}
+
+// MsgCreateScavenge - struct for unjailing jailed validator
+type MsgCreateScavenge struct {
+	Creator      sdk.AccAddress `json:"creator" yaml:"creator"`
+	Description  string         `json:"description" yaml:"creator"`
+	SolutionHash string         `json:"solutionHash" yaml:"solutionHash"`
+	Reward       sdk.Coins      `json:"reward" yaml:"reward"`
+}
+
+func NewMsgCreateScavenge(creator sdk.AccAddress, description string, solutionHash string, reward sdk.Coins) MsgCreateScavenge {
+	return MsgCreateScavenge{Creator: creator, Description: description, SolutionHash: solutionHash, Reward: reward}
+}
+
+const CreateScavengeConst = "CreateScavenge"
+
+// nolint
+func (msg MsgCreateScavenge) Route() string { return RouterKey }
+func (msg MsgCreateScavenge) Type() string  { return CreateScavengeConst }
+func (msg MsgCreateScavenge) GetSigners() []sdk.AccAddress {
+	return []sdk.AccAddress{sdk.AccAddress(msg.Creator)}
+}
+
+// GetSignBytes gets the bytes for the message signer to sign on
+func (msg MsgCreateScavenge) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+// ValidateBasic validity check for the AnteHandler
+func (msg MsgCreateScavenge) ValidateBasic() error {
+	if msg.Creator.Empty() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "creator can't be empty")
+	}
+	if msg.SolutionHash == "" {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "solutionScavengerHash can't be empty")
+	}
+	return nil
+}
