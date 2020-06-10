65c80fe9cf6a77c878981d76f0d42ffcb986cbf2
OTSUKI Shinsuke <otsuki.shinsuke@gmail.com>
2020-06-10 10:33:34 +0900

    step05: Keeper

--- x/scavenge/types/key.go
+++ x/scavenge/types/key.go
@@ -7,9 +7,12 @@ const (
 	// StoreKey to be used when creating the KVStore
 	StoreKey = ModuleName
 
-  // RouterKey to be used for routing msgs
-  RouterKey = ModuleName
+	// RouterKey to be used for routing msgs
+	RouterKey = ModuleName
 
-  // QuerierRoute to be used for querierer msgs
+	// QuerierRoute to be used for querierer msgs
 	QuerierRoute = ModuleName
+
+	ScavengePrefix = "sk-"
+	CommitPrefix   = "ck-"
 )
