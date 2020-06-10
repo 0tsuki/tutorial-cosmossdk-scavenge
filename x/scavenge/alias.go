39b34929a9f60759ce50b6bb829468c4a9c0491f
OTSUKI Shinsuke <otsuki.shinsuke@gmail.com>
2020-06-10 09:59:10 +0900

    step04: Imple alias

--- x/scavenge/alias.go
+++ x/scavenge/alias.go
@@ -6,13 +6,10 @@ import (
 )
 
 const (
-	// TODO: define constants that you would like exposed from your module
-
 	ModuleName        = types.ModuleName
 	RouterKey         = types.RouterKey
 	StoreKey          = types.StoreKey
 	DefaultParamspace = types.DefaultParamspace
-	QueryParams       = types.QueryParams
 	QuerierRoute      = types.QuerierRoute
 )
 
@@ -24,11 +21,13 @@ var (
 	NewGenesisState     = types.NewGenesisState
 	DefaultGenesisState = types.DefaultGenesisState
 	ValidateGenesis     = types.ValidateGenesis
-	// TODO: Fill out function aliases
 
 	// variable aliases
-	ModuleCdc     = types.ModuleCdc
-	// TODO: Fill out variable aliases
+	ModuleCdc = types.ModuleCdc
+
+	NewMsgCreateScavenge = types.NewMsgCreateScavenge
+	NewMsgCommitSolution = types.NewMsgCommitSolution
+	NewMsgRevealSolution = types.NewMsgRevealSolution
 )
 
 type (
@@ -36,5 +35,7 @@ type (
 	GenesisState = types.GenesisState
 	Params       = types.Params
 
-	// TODO: Fill out module types
+	MsgCreateScavenge = types.MsgCreateScavenge
+	MsgCommitSolution = types.MsgCommitSolution
+	MsgRevealSolution = types.MsgRevealSolution
 )
